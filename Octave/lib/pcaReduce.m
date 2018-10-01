c1a291b972046fdf35898ac2deaaa341dd65391a
Ray Comas <ray.comas@stratusinnovations.com>
2018-09-30 23:32:47 -0400

    Updated PCA function

--- Octave/lib/pcaReduce.m
+++ Octave/lib/pcaReduce.m
@@ -1,14 +1,15 @@
-function [Z W] = pcaReduce(X, ret_var = 0.99)
+function [Z W S] = pcaReduce(X, ret_var = 0.99)
 %pcaReduce  Perform Principal Component Analysis on X
-%  [Z W] = pcaReduce(X)
+%  [Z W S] = pcaReduce(X, ret_var)
 %  Performs Principal Component Analysis on X and reduce its dimensionality
 %    X        the data to analyze. Each column corresponds to one set of
 %             observations. The data should alreedy be normalized so that
 %             all the values have similar order of magnitude
-%    ret_var  the desired retained variance
+%    ret_var  the desired retained variance (default 0.99)
 %  Returns
 %    Z      the trasform of X 
-%    W      the matrix that was used to transform X (by computing X * U_red)
+%    W      the matrix that was used to transform X (by computing X * W)
+%    S      The variance percentages for each column of W
 %
 % Copyright (C) 2018 Ray Comas
 %
@@ -26,5 +27,6 @@ function [Z W] = pcaReduce(X, ret_var = 0.99)
 
   W = U(:,1:k);
   Z = X * W;
+  S = s_diag(1:k) ./ var_tot;
   
 endfunction
